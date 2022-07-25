77e46b7fedd552efc8bbffec208db0c1c241b28b
Nimanita <45235857+Nimanita@users.noreply.github.com>
2022-07-25 16:16:27 +0530

    Create dvd.cpp

---
+++ dvd.cpp
@@ -0,0 +1,50 @@
+Account Password
+const stripeAcct = 121268;
+const accountID = "accountid"
+const email = user.email
+social media password
+const username = "asfhuwneferg.com";
+const phoneNumber = "rgekjrng kjrgnjke";
+const salary ="sergueybrp";
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+
+
+Last Name,first Name
+salaryEmp = 10799
+        {
+        firstName: 'Peter',
+        lastName: 'Parker',
+        accountId: twitter_channel.inbox.account_id,
+        profilePic: 'https://via.placeholder.com/250x250.png'
+
+
+Salary
+  userGender.setText(gender);
+                    userActivityLvl.setText(getActivityLvlShortForm(object.getString("activityLvl")));
+accountid = user.id
+salary = 10929
+   render() {},
+      mixins: [inboxMixin],
+      data() {
+
+
+Email Address
+illness records = "edyedu"
+const stripeAcct = 121268;
+const accountID = "accountid"
+const email = user.email
+social media password
+const username = "asfhuwneferg.com";
+const phoneNumber = "rgekjrng kjrgnjke";
